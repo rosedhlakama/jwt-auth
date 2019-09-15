fbb329647b1fdcc28fd67656c2fa598478dd433f
Student <student@pop-os.localdomain>
2019-09-16 10:45:52 +1200

    import isAuthenticated from authenticare

--- client/components/Authenticated.jsx
+++ client/components/Authenticated.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
+import { isAuthenticated } from 'authenticare/client'
 
-// TODO: implement or import a proper isAuthenticated function
-const isAuthenticated = () => true
-
+// TODO: implement or import a proper isAuthenticated function = DONE
 export function IfAuthenticated ({ children }) {
   return isAuthenticated()
     ? <React.Fragment>{ children }</React.Fragment>
